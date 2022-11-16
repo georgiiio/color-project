99f260b04d2ee0b6c250bc7a7e34e0c0d5980b31
Georgiiio <gosha.pronkin@mail.ru>
2022-11-16 15:27:31 +0100

    add fuctionality to the lock

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
 const cols = document.querySelectorAll('.col')
 
 document.addEventListener('keydown', (event) => {
+  event.preventDefault()
   if (event.code == 'Space') {
     setRandomColor()
   }
 })
 
+document.addEventListener('click', event => {
+  const type = event.target.dataset.type
+
+  if (type == 'lock'){
+    const node = event.target.tagName.toLowerCase() == 'i'
+      ? event.target
+      : event.target.children[0]
+
+    node.classList.toggle('fa-lock-open')
+    node.classList.toggle('fa-lock')
+  }
+})
+
 function setRandomColor(){
   cols.forEach((col) => {
+    const isLocked = col.querySelector('i').classList.contains('fa-lock')
     const text = col.querySelector('h2')
     const button = col.querySelector('button')
     const color = chroma.random()
+
+    if (isLocked){
+      return
+    }
 
     text.textContent = color
     col.style.background = color
